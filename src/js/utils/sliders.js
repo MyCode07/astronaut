883a82d48cbca4e4cd4fdf84b1e2d203d8753162
MyCode07 <erem19992015@mail.ru>
2025-08-12 12:10:00 +0300

    start add casino section

--- src/js/utils/sliders.js
+++ src/js/utils/sliders.js
@@ -1,111 +1,25 @@
-import { Swiper, Autoplay, Lazy } from "swiper";
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
 
-const casinoSlider = document.querySelector('.casino-slider .swiper');
-const casinoSliderSlides = document.querySelectorAll('.casino-slider .swiper .swiper-slide');
+const casinoSlider = document.querySelectorAll('.swiper');
 
-if (casinoSliderSlides.length) {
-    new Swiper(casinoSlider, {
-        modules: [Autoplay, Lazy],
-        loop: false,
-        slidesPerView: 'auto',
-        autoplay: {
-            delay: 1000,
-            pauseOnMouseEnter: true,
-        },
-        speed: 700,
+if (casinoSlider.length) {
+    casinoSlider.forEach(slider => {
+        const section = slider.closest('section')
+        const prev = section.querySelector('.prev')
+        const next = section.querySelector('.next')
+        const pagination = section.querySelector('.pagination')
 
-        preloadImages: false,
-        lazy: {
-            loadPrevNext: true,
-            loadOnTransitionStart: false
-        },
-        watchSlidesProgress: true,
-        watchOverflow: true,
-        grabCursor: true,
-
-        breakpoints: {
-            300: {
-                spaceBetween: 15,
-                centeredSlides: true,
-            },
-            600: {
-                spaceBetween: 20,
-                centeredSlides: false,
-            }
+        if (slider.closest('.adv') && window.innerWidth <= 768) {
+            new Swiper(slider, {
+                modules: [Navigation],
+                slidesPerView: 'auto',
+                spaceBetween: 8,
+                navigation: {
+                    prevEl: prev,
+                    nextEl: next,
+                }
+            })
         }
     })
-}
-
-
-const rouletteSlider = document.querySelector('.roulette-slider .swiper');
-const rouletteSliderSlides = document.querySelectorAll('.roulette-slider .swiper .swiper-slide');
-
-if (rouletteSliderSlides.length) {
-    new Swiper(rouletteSlider, {
-        modules: [Autoplay],
-        loop: false,
-        slidesPerView: 'auto',
-        autoplay: {
-            delay: 1000,
-            pauseOnMouseEnter: true,
-        },
-        speed: 700,
-
-        preloadImages: false,
-        lazy: {
-            loadPrevNext: true,
-            loadOnTransitionStart: false
-        },
-        watchSlidesProgress: true,
-        watchOverflow: true,
-        grabCursor: true,
-
-        breakpoints: {
-            300: {
-                spaceBetween: 15,
-                centeredSlides: true,
-            },
-            600: {
-                spaceBetween: 20,
-                centeredSlides: false,
-            }
-        }
-    })
-}
-
-
-const darkSlider = document.querySelector('.dark-slider .swiper');
-const darkSliderSlides = document.querySelectorAll('.dark-slider .swiper .swiper-slide');
-
-if (darkSliderSlides.length) {
-    new Swiper(darkSlider, {
-        modules: [Autoplay],
-        loop: false,
-        slidesPerView: 'auto',
-        autoplay: {
-            delay: 1000,
-            pauseOnMouseEnter: true,
-        },
-        speed: 700,
-
-        preloadImages: false,
-        lazy: {
-            loadPrevNext: true,
-            loadOnTransitionStart: false
-        },
-        watchSlidesProgress: true,
-        watchOverflow: true,
-        grabCursor: true,
-
-        breakpoints: {
-            300: {
-                spaceBetween: 15,
-                centeredSlides: true,
-            },
-            600: {
-                spaceBetween: 20,
-                centeredSlides: false,
-            }
-        }
-    })
-}
+}   
